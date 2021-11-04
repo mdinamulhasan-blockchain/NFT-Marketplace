88b7f11ed10af5ce0e71992b9f6b59e319d61269
Md Inamul Hasan <85017600+mdinamulhasan-blockchain@users.noreply.github.com>
2021-11-04 10:14:49 +0530

    Update MarketPlace.sol

--- MarketPlace.sol
+++ MarketPlace.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: Unlicensed
 
-pragma solidity 0.8.4;
+pragma solidity ^0.8.4;
 
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC721/IERC721.sol";  //REMIX IDE
 
